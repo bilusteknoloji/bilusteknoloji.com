c5849dfcb73c2853671418260b69e6c3c3b8ac3e
Uğur Özyılmazel <vigo@bilusteknoloji.com>
2025-04-05 01:00:47 +0300

    release 2025-04-05-01-00

--- public/js/site.js
+++ public/js/site.js
@@ -1,18 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const items = document.querySelectorAll('.reveal');
-
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                const el = entry.target;
-                const index = Array.from(items).indexOf(el);
-
-                el.classList.remove('opacity-0', 'translate-y-4');
-                el.classList.add(`animate-fade-in-up-${index + 1}`);
-                observer.unobserve(el);
-            }
-        });
-    }, {threshold: 0.1});
-
-    document.querySelectorAll('.reveal').forEach(el => observer.observe(el));
-});
+document.addEventListener("DOMContentLoaded",()=>{const e=document.querySelectorAll(".reveal"),t=new IntersectionObserver(r=>{r.forEach(r=>{if(r.isIntersecting){const n=r.target,o=Array.from(e).indexOf(n);n.classList.remove("opacity-0","translate-y-4"),n.classList.add(`animate-fade-in-up-${o+1}`),t.unobserve(n)}})},{threshold:.1});document.querySelectorAll(".reveal").forEach(e=>t.observe(e))});
